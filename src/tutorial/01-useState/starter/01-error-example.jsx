6ba271ad6a49ae2968706ebff105a7c0dc9562f4
Gbolahan Sholola <sholola.gbolahan@gurugeeksroyalty.biz>
2024-04-13 17:10:36 +0100

    Initited useState for rendering count

--- src/tutorial/01-useState/starter/01-error-example.jsx
+++ src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,26 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+
+
+  //  Page not updating count on button click because page is not rerendering 
+
+  // UseSate hooks is used to solve the problem
+  
+  let count = 0;
+
+  const handleCLick = ( ) => {
+      count = count + 1
+      console.log(count)
+  }
+
+  return(
+  <>
+    <h2>{count}</h2>
+
+    <button type="button"  className="btn" onClick={handleCLick}>
+      icrease
+    </button>
+  </>
+  )
 };
 
 export default ErrorExample;
--- src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+
+const [count, setCount] = useState(0)
+
+const handleClick = () =>{
+  setCount (count + 1)
+
+
+}
+
+  return (
+
+    <div>
+      <h1> You clicked {count} times</h1>
+      <button className="btn" onClick={handleClick}> click me </button>
+    </div>
+
+  )
 };
 
 export default UseStateBasics;
