a2e207d8bb733f1a31fc51d437dff2d120b66a3b
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-06-21 14:25:34 +0100

    Checkbox inputs

--- src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -1,24 +1,40 @@
-import { useState } from 'react';
-const frameworks = ['react', 'angular', 'vue', 'svelte'];
+import { useState } from "react"
+const frameworks = ["react", "angular", "vue", "svelte"]
+
 const OtherInputs = () => {
+  // Checkbox can either be true / false, so default value is set to False
+  const [shipping, setShipping] = useState(false)
+
+  const handleShipping = (e) => {
+    console.log(e.target.checked)
+
+    setShipping(e.target.checked)
+  }
   return (
     <div>
-      <form className='form'>
+      <form className="form">
         <h4>Other Inputs</h4>
         {/* name */}
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='shipping'> Free Shipping </label>
+        <div className="form-row" style={{ textAlign: "left" }}>
+          <label htmlFor="shipping"> Free Shipping </label>
+          <input
+            type="checkbox"
+            name="shipping"
+            //Checked is used instead of value in a checkbox input
+            checked={shipping}
+            onChange={handleShipping}
+          />
         </div>
-        <div className='form-row' style={{ textAlign: 'left' }}>
-          <label htmlFor='framework' className='form-label'>
+        <div className="form-row" style={{ textAlign: "left" }}>
+          <label htmlFor="framework" className="form-label">
             Framework
           </label>
         </div>
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
     </div>
-  );
-};
-export default OtherInputs;
+  )
+}
+export default OtherInputs
