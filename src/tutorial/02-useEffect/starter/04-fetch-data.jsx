c78ef7730e38c5032e211425dcada07bc365d62f
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-13 21:30:22 +0100

    Use custom hooks to fetch data

--- src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,44 +1,39 @@
-const url = "https://api.github.com/users";
-import React, { useEffect, useState } from "react";
+const url = "https://api.github.com/users"
+import useFetchPerson from "../../08-custom-hooks/starter/useFetchPerson"
 
 const FetchData = () => {
-  const [user, setUsers] = useState([]) ;
+  const { isError, isLoading, user } = useFetchPerson(url)
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-
-        const users = await response.json( );
-        setUsers(users);
-        console.log(users);
-      } catch (err) {
-        console.log(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+  if (isError) {
+    return (
+      <div>
+        <h2>An Error as occured</h2>
+      </div>
+    )
+  }
   return (
     <>
       <h1> Github Users </h1>
-
-      <div className="users_container">
-      {user.map(({ login, avatar_url, html_url, id, node_id }) => {
-        return (
-          
-            <div key={id} className="user_container">
-              <img src={avatar_url} alt={node_id} className="user_img" />
-              <div className="user_info">
-                <h5>{login}</h5>
-                <a href={html_url} target="_blank">Profile</a>
+      {isLoading ? (
+        "Loading...."
+      ) : (
+        <div className="users_container">
+          {user.map(({ login, avatar_url, html_url, id, node_id }) => {
+            return (
+              <div key={id} className="user_container">
+                <img src={avatar_url} alt={node_id} className="user_img" />
+                <div className="user_info">
+                  <h5>{login}</h5>
+                  <a href={html_url} target="_blank">
+                    Profile
+                  </a>
+                </div>
               </div>
-            </div>  
-        );
-      })}
-      </div>
+            )
+          })}
+        </div>
+      )}
     </>
-  );
-};
-export default FetchData;
+  )
+}
+export default FetchData
