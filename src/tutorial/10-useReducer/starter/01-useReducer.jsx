bf6aafceb60e68798c74b17df6168ea2ec4a1592
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-19 23:40:18 +0100

    Throwing Error message if no action found

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,15 +1,42 @@
-import React from "react"
+import React, { useReducer, useState } from "react"
 import { data } from "../../../data"
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+}
+
+const CLEAR_LIST = "CLEAR_LIST"
+const REMOVE_ITEM = "REMOVE_ITEM"
+const RESET_LIST = "RESET_LIST"
+
+const reducer = (state, action) => {
+  if (action.type === CLEAR_LIST) {
+    return { ...state, people: [] }
+  }
+
+  if (action.type === RESET_LIST) {
+    return { ...state, people: [data] }
+  }
+  //  This is to throw an error when an there's an unknown action called
+  throw new Error(`No Matching " ${action.type} " - action-type `)
+
+  //  This is to return State when no action is found
+  return state
+}
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data)
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id)
-    setPeople(newPeople)
+    // let newPeople = people.filter((person) => person.id !== id)
+    // setPeople(newPeople)
   }
+
+  console.log(state)
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person
         return (
           <div key={id} className="item">
@@ -18,16 +45,16 @@ const ReducerBasics = () => {
           </div>
         )
       })}
-      {people.length > 0 ? (
+      {state.people.length > 0 ? (
         <button
           className="btn"
           style={{ marginTop: "2rem" }}
-          onClick={() => setPeople([])}
+          onClick={() => dispatch({ type: CLEAR_LIST })}
         >
           clear items
         </button>
       ) : (
-        <button onClick={() => setPeople(data)} className="btn">
+        <button onClick={() => dispatch({ type: "somthimg" })} className="btn">
           reset
         </button>
       )}
