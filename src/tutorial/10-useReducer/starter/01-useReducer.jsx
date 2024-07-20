2ab73209ba61fe9b14ac460d9d020dcdf25a9b17
Sholola-Gbolahan <shololagbolahan@gmail.com>
2024-07-20 09:27:35 +0100

    Accessing reducer and actions from external file making code base clean

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,36 +1,12 @@
 import React, { useReducer, useState } from "react"
 import { data } from "../../../data"
 
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./actions"
+import reducer from "./reducer"
+
 const defaultState = {
   people: data,
   isLoading: false,
-}
-
-const CLEAR_LIST = "CLEAR_LIST"
-const REMOVE_ITEM = "REMOVE_ITEM"
-const RESET_LIST = "RESET_LIST"
-
-const reducer = (state, action) => {
-  if (action.type === CLEAR_LIST) {
-    return { ...state, people: [] }
-  }
-
-  if (action.type === RESET_LIST) {
-    return { ...state, people: data }
-  }
-
-  if (action.type === REMOVE_ITEM) {
-    const newPeople = state.people.filter((person) => {
-      return person.id !== action.payload.id
-    })
-
-    return { ...state, people: newPeople }
-  }
-
-  throw new Error(`Error with "${action.type}" action type`)
-
-  //  This is to return State when no action is found
-  // return state
 }
 
 const ReducerBasics = () => {
